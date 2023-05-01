31f2dfcfe44c8abebebcb51c7cb4605347aafdf7
teamskiy <teamskiy@gmail.com>
2023-05-02 01:33:24 +0200

    start of a web app that i hope i will use

---
+++ src/main/java/teamskiy/eventtracker/EventTrackerApplication.java
@@ -0,0 +1,13 @@
+package teamskiy.eventtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teamskiy/eventtracker/EventTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package teamskiy.eventtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
